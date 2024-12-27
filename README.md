bdf7b8fbdaa5813696f3336063c868fc78232e7c
crossjane <cross.jane.kim@gmail.com>
2024-12-27 16:33:41 +0900

    24.12.27

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# 공부한점
+# Javascript 이해도 높이기
+
+프로젝트 목표: 
+
+1. React를 배우면서 기초가가 부족하다고 느껴, 느낌으로만 배웠던 JavaScript의 심도깊은은 이해를 위해  DOM 조작 하는 방법으로 다시 구현해보았습니다. 
+
+- JavaScript의 DOM API를 활용하여 할 일(To-Do)을 추가, 수정, 삭제기능 구현
+- createElement, append, addEventListener 등의 DOM 조작 메서드를 사용해 직접 HTML 요소를 생성하고 이벤트 핸들러를 연결
+- 체크박스 상태에 따라 수정/삭제 버튼을 표시하거나 숨기는 로직을 구현
+
+2. 잘 몰랐던 Promise와 Async/Await를 이용한 비동기 처리 이해
+
+- Promise와 async/await를 활용해 비동기 작업 처리하는 방법을 이해하고, 이론적으로 비동기에 대한 이해도를 높임
+- setTimeout과 같은 비동기 함수와 동기적으로 보이는 코드를 써서 이론적으로 연습하여 학습
--- index.js
+++ index.js
@@ -7,18 +7,27 @@ function add() {
    }
 
    const value = input.value;
-   //굳이 해주는 이유?
 
    if(value.trim()=== ""){
     alert
    }
+
+   //체크박스
+   const checkbox = document.createElement("input");
+   checkbox.type="checkbox";
+   checkbox.className ="checkbox-style";
+   checkbox.id = "checkboxId";
+
+   const checkboxLabel = document.createElement("label");
+   checkboxLabel.htmlFor = "checkboxId";
+
 
 
    const li = document.createElement("li");
    li.className="todo-item";
 
    const span = document.createElement("span");
-   span.innerText=value; //value여기에서 쓰려고?
+   span.innerText=value; 
 
    const button = document.createElement("button");
    button.className="update-btn";
@@ -29,7 +38,23 @@ function add() {
    
    button2.className="delete-btn";
    button2.innerText="삭제";
+   button.id = "deleteButton";
 
+   button.style.display="none";
+   button2.style.display="none";
+
+   checkbox.addEventListener("change", function(){
+      if(checkbox.checked){
+         button.style.display="inline-block";
+         button2.style.display="inline-block";
+      }else{
+         button.style.display="none";
+         button2.style.display="none";
+      }
+   })
+
+   li.append(checkbox);
+   li.append(checkboxLabel);
    li.append(span);
    li.append(button);
    li.append(button2);
@@ -39,14 +64,15 @@ function add() {
       onClickUpdate(li, span, button);
    });
 
-
-
    //삭제
    button2.addEventListener("click", deleteItem);
 
    const ul = document.querySelector("#todo-list");
    ul.append(li);
    input.value=""
+
+ 
+
 
 }
 
@@ -62,15 +88,23 @@ function onClickUpdate(li, span, button){
    span.style.display="none";
    button.style.display="none";
 
+   const checkbox = li.querySelector("input[type='checkbox']"); 
+   checkbox.checked = false; 
+
+   const checkboxLabel = li.querySelector("label");
+   checkboxLabel.style.display = "none";
+
 
    const input_update = document.createElement("input");
    input_update.value = span.innerText;
+   input_update.className="input-update";
 
    
 
    const buttonDone = document.createElement("button");
    buttonDone.className="update-btn";
    buttonDone.innerText="완료";
+   checkbox.checked = false; 
 
    buttonDone.addEventListener("click", updateItem);
    
@@ -86,6 +120,9 @@ function updateItem(){
    const span = this.parentElement.getElementsByTagName("span")[0];
    const input_update = this.parentElement.getElementsByTagName("input")[0];
    const button = this.parentElement.querySelector("#updateButton");
+   const button2 = this.parentElement.querySelector("#deleteButton");
+   const checkbox = this.parentElement.querySelector("input[type='checkbox']");
+   const checkboxLabel = this.parentElement.querySelector("label");
 
    span.innerText=input_update.value;
    span.style.display="inline";
@@ -93,7 +130,108 @@ function updateItem(){
 
    input_update.remove();
    this.remove();
-   button.style.display="block";
+   // button.style.display="block";
+
+
+   checkbox.checked = false; 
+   checkboxLabel.style.display = "inline-block"; 
+
+   button.style.display="none";
+   button2.style.display="none";
 }
+
+
+
+console.log("11111");
+
+function getData(){
+
+   return new Promise(function(resolve,reject){
+      setTimeout(function(){
+         resolve("2222");
+      },0);
+   });
+}
+
+getData().then(function(result){
+   console.log()
+
+})
+
+
+function getData(){
+
+   return new Promise(function(resolve, reject){
+      setTimeout(function() {
+         resolve("22222");
+   }, 0);
+  
+   
+   });
+
+}
+
+
+console.log("33333");
+
+
+
+
+function getData(){
+   return new Promise(function(resolve,reject){
+      setTimeout(function(){
+         resolve("2222")
+      },0)
+   })
+}
+
+
+async function asyncGetData(){
+   await getData();
+
+}
+
+
+
+// function getData() {
+
+//    return new Promise(function(reslove, reject) {
+//       setTimeout(function() {
+//          reslove("22222222")
+//       }, 0);
+//    });
+
+// }
+
+// function getData2() {
+//    setTimeout(function() {
+//       console.log("2222222")
+//    }, 0)
+// }
+
+// async function asyncGetData() {
+//    await getData();
+//    await getData2();
+//    console.log("3333333");
+// }
+
+// console.log("11111111");
+
+// getData2()
+
+
+// getData().then(function(result) {
+//    console.log(result);
+//    console.log("333333");
+// }).catch(function(error) {
+//    console.log(error);
+// })
+
+
+
+
+
+
+
 
 
